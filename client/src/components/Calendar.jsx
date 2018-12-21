ca454aaba231fb224c1ecb26addbf1107f500254
Kevin Li <li.ctrl.z@gmail.com>
2018-12-21 10:49:47 -0800

    Implement barebones calendar (no controller)

--- client/src/components/Calendar.jsx
+++ client/src/components/Calendar.jsx
@@ -1,7 +1,65 @@
 import React from 'react';
 
-const Calendar = (props) => {
-  <div>This is the calendar</div>
-};
+const Calendar = () => (
+  <body className='Calendar Module'>
+    <div className='Availability'>Availability</div>
+    <div className='minStay'>'(place holder text) 1 min night stay - updated 1 month ago</div>
+    <div className='Clear dates'>Clear dates</div>
+
+    <div class='month'> 
+      <ul>
+        <li class='prev'>PREV</li>
+        <li class='next'>NEXT</li>
+        <li>December<br></br><span>2018</span></li>
+      </ul>
+    </div>
+
+    <ul class='weekdays'>
+      <li>Mo</li>
+      <li>Tu</li>
+      <li>We</li>
+      <li>Th</li>
+      <li>Fr</li>
+      <li>Sa</li>
+      <li>Su</li>
+    </ul>
+
+    <ul class='days'> 
+      <li>1</li>
+      <li>2</li>
+      <li>3</li>
+      <li>4</li>
+      <li>5</li>
+      <li>6</li>
+      <li>7</li>
+      <li>8</li>
+      <li>9</li>
+      <li><span className='active'>10</span></li>
+      <li><span className='active'>11</span></li>
+      <li><span className='active'>12</span></li>
+      <li><span className='active'>13</span></li>
+      <li>14</li>
+      <li>15</li>
+      <li>16</li>
+      <li>17</li>
+      <li>18</li>
+      <li>19</li>
+      <li>20</li>
+      <li>21</li>
+      <li>22</li>
+      <li>23</li>
+      <li>24</li>
+      <li>25</li>
+      <li>26</li>
+      <li>27</li>
+      <li>28</li>
+      <li>29</li>
+      <li>30</li>
+    </ul> 
+
+  </body>
+);
+
+
 
 export default Calendar;
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,14 +4,46 @@ import Calendar from './components/Calendar.jsx';
 
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      bookedDates: null
+    };
+
+  }
+
+
+numOfDays(mm, yyyy) {
+  let daysOfMonth;
+  if (mm === 4 || mm === 6 || mm === 9 || mm === 11) {
+    daysOfMonth = 30;
+  } else if (mm === 2) {
+    if (yyyy/4 - parseInt(yyyy/4) !== 0) {
+      daysOfMonth = 28;
+    } else if (yyyy/100 - parseInt(yyyy/100) !== 0) {
+      daysofmonth = 29
+    } else if (yyyy/400 - parseInt(yyyy/400) !== 0) {
+      daysOfMonth = 28;
+    } else {
+      daysOfMonth = 29;
+    }
+  } else {
+  daysOfMonth = 31;
+  }
+  
+  return daysOfMonth;
+}
+
+
   render() {
     return (
       <div>
-        poo
+        <Calendar />
       </div>
     )
   };
-}
+};
 
 ReactDOM.render(
   <App />,
